e7bfc3ccdab0eb65c67b6a8cc83250ca9c0a4873
TN-1 <trust-no-1@adam.com.au>
2015-04-18 15:51:29 +0930

    Updated ged.c

--- ged.c
+++ ged.c
@@ -1,20 +1,17 @@
 /*
-    GED3, A lightweight text editor built on GTK3.
-    This is a derivative work of GED  https://github.com/8l/ged
-    Copyright (C) 2015  Hamish West
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  ged - a lightweight GTK text editor
+  Copyright (C) 2005 Ben Good, James Hartzell
+  This program is free software; you can redistribute it and/or modify it 
+  under the terms of the GNU General Public License as published by the
+  Free Software Foundation; either version 2 of the Licesnse, or (at your
+  option) any later version.
+  This program is distributed in the hope that it will be useful but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+  See the GNU General Public License for more details. You should have
+  received a copy of the GNU General Public License along with this 
+  program; if not, write to the Free Software Foundaton, Inc.,
+  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 /*Standard C library includes*/
 #include <stdarg.h>
